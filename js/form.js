1df7b0f62ebcc98f6786d57e8ff728bd1951944e
Poy Woo <poywoo@Poys-MacBook-Pro.local>
2017-05-21 00:55:56 -0700

    Loading screen on send

--- js/form.js
+++ js/form.js
@@ -1,7 +1,11 @@
 $("form").submit(function(event) {
 	if ($("#firstName").val() && $("#lastName").val() && $("#email").val() && $("#nonprofitName").val() && $("#website").val() &&
 		$("#phoneNumber").val() && $("#twitterUsername").val()) {
-			$("#sentModal").modal("show");
+    		$('.overlay').show();
+    		setTimeout(function(){ 
+    			$('.overlay').hide();
+    			$("#sentModal").modal("show"); 
+    		}, 3000);
 	} else {
 		alert("Please fill out all fields before sending the form");
 	}
